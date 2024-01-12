4024071bf561b61aa5a1f02f21e4d4d8e0fe5776
k2omega7@outlook.jp <k2omega7@outlook.jp>
2024-01-12 14:49:45 +0900

    add Create script in create/route.js

--- app/api/item/create/route.js
+++ app/api/item/create/route.js
@@ -1,8 +1,19 @@
 // app/api/create/route.js
 
 import { NextResponse } from "next/server";
+import connectDB from "@/app/utils/database";
+import { ItemModel } from "@/app/utils/schemaModels";
 
 export async function POST(request){
-    console.log(await request.json());
-    return NextResponse.json({msg : "アイテム作成"});
+
+    const reqBody = await request.json();
+
+    try {
+        await connectDB();
+        await ItemModel.create(reqBody);
+        return NextResponse.json({msg : "アイテム作成成功"});
+    } catch (error) {
+        return NextResponse.json({msg : "アイテム作成失敗"});
+    }
+
 }
