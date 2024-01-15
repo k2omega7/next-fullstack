2966a8bc3a0882684ac23692edca923b0d97eeaa
k2omega7@outlook.jp <k2omega7@outlook.jp>
2024-01-15 16:18:28 +0900

    add frontend init files

---
+++ app/user/login/page.js
@@ -0,0 +1,59 @@
+// app/login/page.js
+
+"use client";
+import { useState } from "react";
+
+const Login = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch("http://localhost:3000/api/user/login", {
+                method:"POST",
+                headers: {
+                    "Accept":"application/json",
+                    "Content-Type":"application/json",
+                },
+                body: JSON.stringify({
+                    email: email,
+                    password: password,
+                })
+            });
+            const jsonData = await response.json();
+            alert(jsonData.msg);
+        } catch (error) {
+            alert("ユーザ登録失敗");
+            console.log(error);
+        }
+    };
+
+    return(
+        <div>
+            <h1>ユーザ登録</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    value={email}
+                    onChange={(e) => {setEmail(e.target.value);}}
+                    type="text"
+                    name="email"
+                    placeholder="email"
+                    required
+                /><br/>
+                <input
+                    value={password}
+                    onChange={(e) => {setPassword(e.target.value);}}
+                    type="text"
+                    name="password"
+                    placeholder="password"
+                    required
+                /><br/>
+                <button>ログイン</button>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
---
+++ app/user/register/page.js
@@ -0,0 +1,69 @@
+// app/register/page.js
+
+"use client";
+import { useState } from "react";
+
+const Register = () => {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch("http://localhost:3000/api/user/register", {
+                method:"POST",
+                headers: {
+                    "Accept":"application/json",
+                    "Content-Type":"application/json",
+                },
+                body: JSON.stringify({
+                    name: name,
+                    email: email,
+                    password: password,
+                })
+            });
+            const jsonData = await response.json();
+            alert(jsonData.msg);
+        } catch (error) {
+            alert("ユーザ登録失敗");
+            console.log(error);
+        }
+    };
+
+    return(
+        <div>
+            <h1>ユーザ登録</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    value={name}
+                    onChange={(e) => {setName(e.target.value);}}
+                    type="text"
+                    name="name"
+                    placeholder="name"
+                    required 
+                /><br/>
+                <input
+                    value={email}
+                    onChange={(e) => {setEmail(e.target.value);}}
+                    type="text"
+                    name="email"
+                    placeholder="email"
+                    required
+                /><br/>
+                <input
+                    value={password}
+                    onChange={(e) => {setPassword(e.target.value);}}
+                    type="text"
+                    name="password"
+                    placeholder="password"
+                    required
+                /><br/>
+                <button>登録</button>
+            </form>
+        </div>
+    );
+};
+
+export default Register;
